9f340872a18bd602a165e35e0b3bf3b44325f191
Julia F <46096629+juliafercello@users.noreply.github.com>
2019-03-12 16:24:07 -0500

    working game!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,12 +12,10 @@ firebase.initializeApp(config);
 
 //Global Variables
 var database = firebase.database();
-var numberOfPlayers = database.ref("/numOfPlayers");
-var numberOfConnections = database.ref(".info/connected");
-var allowedPlayers = 2;
 var plays = ["Rock", "Paper", "Scissors"];
-var playerId = 0; 
+var spectatorId = 1;  
 
+//Function to display rock paper scissors buttons
 function displayButtons(player) {
     $("#rpsPlaceholder").empty();
     for (var i = 0; i < plays.length; i++) {
@@ -56,32 +54,11 @@ database.ref("/messages").orderByChild("dateAdded").on("child_added", function (
     console.log("The read failed: " + errorObject.code);
 });
 
-//DO I NEED THIS STILL???  if so, add error thing for read. 
-// When the client's connection state changes...
-
-// numberOfConnections.on("value", function (snapshot) {
-//  if (!snapshot.val()) {
-//      if ((playerId === "1") || (playerId === "2")) {
-//         database.ref("/player" + playerId).remove(); 
-//      }
-    
-//  }
-// });
-    // If they are connected..
-    // if (snapshot.val()) {
-    //     // Add user to the connections list.
-    //     var player = numberOfPlayers.push(true);
-    //     // Remove user from the connection list when they disconnect.
-    //     player.onDisconnect().remove();
-    // }
-//});
-//SAME HERE
-//when a new user arrives, check to see if there are already two players
-// numberOfPlayers.once("value", function (snap) {
-//     if (parseInt(snap.numChildren()) > allowedPlayers) {
-//         alert("nope");
-//     }
-// });
+//When user clears chat history, remove from the page and database
+$("#clearChat").on("click", function () {
+    $("#chatPlaceholder").empty();
+    database.ref("/messages").remove(); 
+});
 
 //start the game by showing a modal for user to enter their name
 $(document).ready(function () {
@@ -93,48 +70,65 @@ $("#submitName").on("click", function (event) {
     event.preventDefault();
 
     var name = $("#playerName").val().trim();
+    if (name === "" ) {
+        name = "B. Secret"
+    }
+
     $("#chatBtn").attr("chatName", name);
 
-    //Assign users to game
+    //Assign users to game and watch for them to leave
     database.ref().once("value", function (snapshot) {
+        //Assign Player 1
         if (!snapshot.child("player1").exists()) {
             database.ref("/player1").set({
                 name: name,
-                score: 0,
+                wins: 0,
+                losses: 0,
                 role: "player1",
                 choice: "none",
             });
-            database.ref("/player1").onDisconnect().remove();
 
-            playerId = "1"; 
             $("#pname").text("Player 1: " + name)
             displayButtons("player1");
             sessionStorage.setItem("role", "player1");
+
+            database.ref("/player1").onDisconnect().remove();
+
         }
+         //Assign Player 2
         else if (!snapshot.child("player2").exists()) {
             database.ref("/player2").set({
                 name: name,
-                score: 0,
+                wins: 0,
+                losses: 0,
                 role: "player2",
                 choice: "none"
             });
 
             database.ref("/player2").onDisconnect().remove();
 
-            playerId = "2"; 
             $("#pname").text("Player 2: " + name)
             displayButtons("player2");
             sessionStorage.setItem("role", "player2");
         }
         else {
-            database.ref("/spectator").push({
+            //User is a spectator
+
+            //Increase number of spectators
+            if (snapshot.child("numOfSpectators").exists()) {
+                spectatorId = parseInt(snapshot.val().numOfSpectators.numOfSpectators) + 1;
+            }
+        
+            database.ref("/numOfSpectators").set({
+                numOfSpectators: spectatorId
+            });
+
+            //Store Spectator Name
+            database.ref("/spectator" + spectatorId).set({
                 name: name
             });
-            
-           // var key = database.ref().child("spectator").push().getKey();
-           // database.ref().child("spectator").child(key).remove(); // This is how you remove it
 
-            //database.ref("/spectator/").onDisconnect().remove();
+            database.ref("/spectator" + spectatorId).onDisconnect().remove();
 
             $("#pname").text("Sorry " + name + ", but you'll have to wait your turn...  The game is already in progress.");
             sessionStorage.setItem("role", "spectator");
@@ -150,19 +144,22 @@ $("#submitName").on("click", function (event) {
 database.ref().on("value", function (snapshot) {
     //Make sure the players exist before checking their score
     if (snapshot.child("player1").exists() && snapshot.child("player2").exists()) {
-    $("#scorePlaceholder").empty();
-    var player1 = snapshot.val().player1.name;
-    var player1Points = snapshot.val().player1.score;
-    var player2 = snapshot.val().player2.name;
-    var player2Points = snapshot.val().player2.score;
+        $("#scorePlaceholder").empty();
+        var player1 = snapshot.val().player1.name;
+        var player1Wins = snapshot.val().player1.wins;
+        var player1Losses = snapshot.val().player1.losses;
 
-    var p1Score = $("<div>");
-    var p2Score = $("<div>");
-    p1Score.text(player1 + "(Player 1): " + player1Points);
-    p2Score.text(player2 + "(Player 2): " + player2Points);
+        var player2 = snapshot.val().player2.name;
+        var player2Wins = snapshot.val().player2.wins;
+        var player2Losses = snapshot.val().player2.losses;
 
-    $("#scorePlaceholder").append(p1Score);
-    $("#scorePlaceholder").append(p2Score);
+        var p1Score = $("<div>");
+        var p2Score = $("<div>");
+        p1Score.text(player1 + "(Player 1): " + "Wins: " + player1Wins + "  Losses: " + player1Losses);
+        p2Score.text(player2 + "(Player 2): " + "Wins: " + player2Wins + "  Losses: " + player2Losses);
+
+        $("#scorePlaceholder").append(p1Score);
+        $("#scorePlaceholder").append(p2Score);
     }
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
@@ -199,7 +196,7 @@ database.ref().on("value", function (snapshot) {
         var player2Choice = snapshot.val().player2.choice;
         var player2Name = snapshot.val().player2.name;
 
-        var result = ""; 
+        var result = "";
 
         if (player1Choice !== "none" && player2Choice !== "none") {
             //tie
@@ -208,65 +205,63 @@ database.ref().on("value", function (snapshot) {
             }
             //player 1 wins
             else if ((player1Choice == "Rock" && player2Choice == "Scissors") || (player1Choice == "Scissors" && player2Choice == "Paper") || (player1Choice == "Paper" && player2Choice == "Rock")) {
-                var p1Score = snapshot.val().player1.score + 1;
+                var p1Wins = snapshot.val().player1.wins + 1;
+                var p2Losses = snapshot.val().player2.losses + 1;
 
                 database.ref("/player1").update({
-                    score: p1Score,
+                    wins: p1Wins,
                     choice: "none"
                 });
 
                 database.ref("/player2").update({
+                    losses: p2Losses,
                     choice: "none"
                 });
-                
+
                 result = player1Name + " wins!";
             }
             //player 2 wins
             else {
-                var p2Score = snapshot.val().player2.score + 1;
+                var p2Wins = snapshot.val().player2.wins + 1;
+                var p1Losses = snapshot.val().player1.losses + 1;
+
                 database.ref("/player2").update({
-                    score: p2Score,
+                    wins: p2Wins,
                     choice: "none"
                 });
-                
+
                 database.ref("/player1").update({
+                    losses: p1Losses,
                     choice: "none"
                 });
-                
+
                 result = player2Name + " wins!";
             };
-        
-        //Show the result of who won/lost
-        $("#theResult").empty(); 
-        var choicesDiv = $("<div>")
-        choicesDiv.html(player1Name + ": " + player1Choice + "<br>" + player2Name + ": " + player2Choice + "<br>")
-        choicesDiv.addClass("lead")
 
-        var resultHdr = $("<h3>")
-        resultHdr.html(result);
-        $("#theResult").append(choicesDiv); 
-        $("#theResult").append(resultHdr);
-        $("#showResultModal").modal('show');
+            //Show the result of who won/lost
+            $("#theResult").empty();
+            var choicesDiv = $("<div>")
+            choicesDiv.html(player1Name + ": " + player1Choice + "<br>" + player2Name + ": " + player2Choice + "<br>")
+            choicesDiv.addClass("lead")
+
+            var resultHdr = $("<h3>")
+            resultHdr.html(result);
+            $("#theResult").append(choicesDiv);
+            $("#theResult").append(resultHdr);
+            $("#showResultModal").modal('show');
         }
     }
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
 
+//when user closes result modal, start a new round if they are player 1 or 2
 $("#closeResult").on("click", function (event) {
     event.preventDefault();
     $("#showResultModal").modal('hide');
 
-    var playerRole = sessionStorage.getItem("role"); 
+    var playerRole = sessionStorage.getItem("role");
     if (playerRole !== "spectator") {
-        displayButtons(playerRole); 
+        displayButtons(playerRole);
     }
-}); 
-
-//    //set total points from snapshot
-//    database.ref().on("value", function (snapshot) {
-//     if (snapshot.child("totalPoints").exists()) {
-//     getReady.totalPoints = snapshot.val().totalPoints;
-
-    //}
-//}); 
+});
